4d3b3276bbbbfa4d96a258e46ebdbe70a9bcce38
Sajedullah Aref <152142322+rf104@users.noreply.github.com>
2024-09-18 22:28:13 +0600

    Update AmplitudeModulationDemodulation.m

--- AmplitudeModulationDemodulation.m
+++ AmplitudeModulationDemodulation.m
@@ -1,70 +1,57 @@
 clear;
 clc;
-%time_parameter
-fs = 10;
-t = linspace(0,1,100);
-%msg_signal
+
+fs = 1000;
+t = linspace(0,1,1000);
+
+%message signal
 fm = 10;
 Am = 1;
-msg_signal = Am*sin(2*pi*fm.*t);
-%carrier_signal
+message_signal = Am*sin(2*pi*fm.*t);
+%carrier signal
 fc = 100;
 Ac = 2;
 carrier_signal = Ac*sin(2*pi*fc.*t);
-%modulated_signal
-modulated_signal = (1+msg_signal).* carrier_signal;
+%modulated signal
+modulated_signal = (1+message_signal).* carrier_signal;
 
-%ploting
+%plotting
 figure;
 subplot(3,1,1);
-plot(t,msg_signal);
-title('MessageSignal');
-xlabel('Time(s)');
-ylabel('Amp');
-grid on;
+plot(t,message_signal);
+title('Message Signal');
 
 subplot(3,1,2);
 plot(t,carrier_signal);
 title('Carrier Signal');
-xlabel('Time(s)');
-ylabel('Amp');
-grid on;
 
 subplot(3,1,3);
 plot(t,modulated_signal);
 title('Modulated Signal');
-xlabel('Time');
-ylabel('Amp');
-grid on;
 
-%demodulation
+%Demodulated
+
+%rectified Signal
+
 rectified_signal = abs(modulated_signal);
 
-%ploting
+%plotting
 figure;
 subplot(2,1,1);
 plot(t,modulated_signal);
-title('ModulatedSignal');
-xlabel('Time(s)');
-ylabel('Amp');
-grid on;
+title('Modulated Signal');
 
 subplot(2,1,2);
 plot(t,rectified_signal);
 title('Rectified Signal');
-xlabel('Time(s)');
-ylabel('Amp');
-grid on;
 
 %lowpass filter parameter
-cuttoff_freq = 20;
-%design lowpassfilter
+cuttoff_frq = 20;
 order = 4;
-nyquist_freq = fs/2;
-normalized_cuttoff_freq = cuttoff_freq/nyquist_freq;
-[b,a] = butter(order,normalized_cuttoff_freq,'low');
+nyquist_frq = fs/2;
+normalized_cuttoff_frq = cuttoff_frq/nyquist_frq;
+[b,a]=butter(order,normalized_cuttoff_frq,'low');
 
-%Apply the low pass  filter to rectified signal
 filtered_signal = filter(b,a,rectified_signal);
 
 %plotting
@@ -72,13 +59,7 @@ figure;
 subplot(2,1,1);
 plot(t,rectified_signal);
 title('Rectified Signal');
-xlabel('Time');
-ylabel('Amp');
-grid on;
 
 subplot(2,1,2);
 plot(t,filtered_signal);
-title('Filterd Signal');
-xlabel('Time(s)');
-ylabel('Amp');
-grid on;
+title('Filtered Signal');
